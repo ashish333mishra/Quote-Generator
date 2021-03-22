b6e7fea358419b5c4dacc0a4b0f8ab8ca26d46b9
Ashish Mishra <ashish.mishra@gslab.com>
2021-03-22 22:22:22 +0530

    Added Twitter functionality

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Global QuotesCollection Array to store API response
 //Accessible accross functions
 let QuotesCollection = [];
-
+let Quote;
+const quoteContainer = document.getElementById("quote");
 /** 
  * function:: getQuotesFromAPI
  * Use:: To Call API and get Quotes to display them on UI
@@ -22,12 +23,52 @@ async function getQuotesFromAPI() {
 
     }
 }
+/**
+ * function:: ShowQuotes
+ * Use:: To access DOM Element and show Quotes and there Auothor Name
+ * Author:: Ashish
+ */
 function showQuotes(){
     //Generate Random Number bw 0 to 1600
-    var Quote = QuotesCollection[Math.floor((Math.random() * QuotesCollection.length))];
-    document.getElementById("quote").innerHTML = Quote.text;
-    document.getElementById("author").innerHTML = Quote.author;
+    Quote = QuotesCollection[Math.floor((Math.random() * QuotesCollection.length))];
+    //If Quote is fetched then show Quote else Alert User
+    if (Quote.text) {
+        //Remove the loader to show Quotes
+        removeLoader();
+        if( Quote.text.length > 35 ) {
+            //Change font size for Responsiveness on Mobile Devices
+            quoteContainer.classList.add('long-quote-text');
+        }
+        //Add the Quote to UI
+        quoteContainer.innerHTML = Quote.text;
+        //Add the Author Name to UI
+        if (Quote.author)
+            document.getElementById("author").innerHTML = Quote.author;
+        else
+        document.getElementById("author").innerHTML = 'Unknown!';
+    }
+    else {
+        alert("Unable to fetch Quotes from API!");
+        console.log(Quote);
+    }
 }
-
+/**
+ * function:: TweetQuote
+ * Use:: To tweet the Quote
+ * Author:: Ashish
+ */
+function TweetQuote(){
+    //Using Twitter API to tweet Quotes
+    const TwitterUrl = `https://twitter.com/intent/tweet?text=${Quote.text} - ${Quote.author}`;
+    window.open(TwitterUrl,'_blank');
+}
+function loader(){
+    document.getElementById('quote-container').hidden = true;
+}
+function removeLoader(){
+    document.getElementById('quote-container').hidden = false;
+    document.getElementById('loader').hidden = true;
+}
 //On Load
+loader();
 getQuotesFromAPI();
